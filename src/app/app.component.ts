200e0cb26cd52dda2d29a2285216f7b2b6d80d0d
DeAndre Johnson <dre@phreshr.com>
2016-09-22 15:14:29 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Fairly Sane';
+  bgImg = '../assets/images/fairly-sane-bg.jpg';
+  bgVid = '../assets/media/Ferris_Wheel_Loop.mp4';
+  logo = {
+    'svg': '../assets/images/fs_logo1.svg',
+    'png': null
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
