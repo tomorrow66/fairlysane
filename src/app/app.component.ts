6ee0605f30b3d89fbd75dee5f93930c0fc4f9691
DeAndre Johnson <dre@phreshr.com>
2016-09-22 17:47:38 -0400

    minor style updates and other edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fairly Sane';
-  bgImg = '../assets/images/fairly-sane-bg.jpg';
-  bgVid = '../assets/media/Ferris_Wheel_Loop.mp4';
+  bgImg = 'assets/images/fairly-sane-bg.jpg';
+  bgVid = 'assets/media/Ferris_Wheel_Loop.mp4';
   logo = {
-    'svg': '../assets/images/fs_logo1.svg',
+    'svg': 'assets/images/fs_logo1.svg',
     'png': null
   };
 }
